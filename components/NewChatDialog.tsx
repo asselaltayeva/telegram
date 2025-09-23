ceee5e45d35735146b42c9343b4bd7d38505987f
Assel <asselaltayeva@users.noreply.github.com>
2025-09-23 22:15:10 +0900

    select and delete user

--- components/NewChatDialog.tsx
+++ components/NewChatDialog.tsx
@@ -13,6 +13,18 @@ export function NewChatDialog( {children} : {children: React.ReactNode}) {
   const {user} = useUser();
   const {setActiveChannel} = useChatContext();
 
+  //select user from the list
+  const handleSelectUser = (user: Doc<"users">) => {
+    // avoid adding the same users twice
+    if (!selectedUsers.find((u) => u._id === user._id)) {
+      setSelectedUsers((prev) => [...prev, user]);
+    }
+  };
+
+  // delete user from the list
+  const removeUser = (userId: string) => {
+   setSelectedUsers((prev) => prev.filter((user) => user._id !== userId));
+  }
     return (
       <>{children}</>
     )
